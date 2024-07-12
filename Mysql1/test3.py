b5f948b3b162b58208240e2ae0ab2b3abcbee3a3
Namdev Patange <154600141+namadevpatange@users.noreply.github.com>
2024-07-12 17:03:36 +0530

    Update test3.py

--- Mysql1/test3.py
+++ Mysql1/test3.py
@@ -2,6 +2,6 @@ import mysql.connector as conn
 
 mydb = conn.connect(host = "localhost" , user ="root" , passwd = "Root@123" )
 cursor = mydb.cursor()
-s = "insert into Test2.student values(101,'vaibhav','mca')"
+s = "insert into Test2.student values(101,'namdev','mca')"
 cursor.execute(s)
 mydb.commit()
